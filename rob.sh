d7d11aaabfd55ec228526123839fa5b0a100b693
m41k <maik.alberto@hotmail.com>
2017-08-16 01:02:10 -0300

    Create rob.sh

---
+++ rob.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cat > roc.html << EOF
+
+<html>
+<title>RobRoc</title>
+<body>
+<script languague="javascript">
+window.open('http://go.oclasrv.com/afu.php?id=1364720')
+window.open('http://go.onclasrv.com/afu.php?id=1364721')
+window.open('http://go.onclasrv.com/afu.php?id=1364722')
+window.open('http://go.oclasrv.com/afu.php?id=1364723')
+window.open('http://go.pub2srv.com/afu.php?id=1364724')
+window.open('http://go.onclasrv.com/afu.php?id=1364725')
+window.open('http://go.onclasrv.com/afu.php?zoneid=1364728')
+window.open('http://go.oclaserver.com/afu.php?zoneid=1364729')
+</script>
+<h1>RobRoc</h1>
+<hr>
+Hi human, I'm bursting the rockets.
+</body>
+</html>
+
+EOF
+
+midori -i 5
+
+RUN=1
+until [ RUN = 1 ];
+ do
+# [ $(free -m | grep Mem | tr -s " " | cut -d " " -f4) -le 100 ] && sysctl -w v$
+  MEM=`free -m | grep Mem | tr -s " " | cut -d " " -f4`
+  if [ $MEM -le 100 ]; then
+  sysctl -w vm.drop_caches=3
+   killall midori
+   sleep 3
+   midori &
+  fi
+ sleep 1
+ done
