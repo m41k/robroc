42652f4d74f38b45b9e0bb6de49c675247050dd7
m41k <maik.alberto@hotmail.com>
2017-08-19 02:06:07 +0000

    Add files via upload

--- robt.sh
+++ robt.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+SITE='54.233.169.1/cgi-bin/log.sh'
+
 while [ 1 ]; do
+
+MEM=`free -m | grep Mem | tr -s " " | cut -d " " -f4`
+  if [ $MEM -le 100 ]; then
+    sysctl -w vm.drop_caches=3
+  fi
+
+BROWSER=(
+'firefox'
+'ie'
+'chorme'
+)
+
+TARRAY=${#BROWSER[@]}
+RANBRO=$(($RANDOM %$TARRAY ))
+UAGENT=${BROWSER[$RANBRO]}
+
+
  /etc/init.d/tor restart
  midori &
- midori -e identify-as=MIDORI_IDENT_CUSTOM -e user-agent=teste -e Go 54.233.169.1/cgi-bin/log.sh
+ sleep 3
+ midori -e identify-as=MIDORI_IDENT_CUSTOM -e user-agent=$UAGENT -e Go $SITE
  sleep 5
  killall midori
  done
